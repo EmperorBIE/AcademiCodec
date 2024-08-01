336ffb69c4f5ed05cf24fed54ce06bea7886683d
EmperorBIE <1286030695@qq.com>
2024-08-01 19:15:44 +0800

    start script

--- egs/SoundStream_24k_240d/start.sh
+++ egs/SoundStream_24k_240d/start.sh
@@ -2,12 +2,12 @@
 source path.sh
 
 python3 main3_ddp.py \
-        --BATCH_SIZE 16 \
+        --BATCH_SIZE 32 \
         --N_EPOCHS 300 \
-        --save_dir path_to_save_log \
-        --PATH  path_to_save_model \
-        --train_data_path path_to_training_data \
-        --valid_data_path path_to_val_data \
-        --sr 24000 \
+        --save_dir /home3/hexin/AcademiCodec/logs \
+        --PATH  /home3/hexin/AcademiCodec/saved_models \
+        --train_data_path /home3/hexin/speechbrain/recipes/VoxLingua107/lang_id/fleurs_train.json \
+        --valid_data_path /home3/hexin/speechbrain/recipes/VoxLingua107/lang_id/fleurs_valid.json \
+        --sr 16000 \
         --ratios 6 5 4 2 \
         --target_bandwidths 1 2 4 8 12
